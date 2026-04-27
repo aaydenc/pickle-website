c00ad7075bec6760cbcd5b09166a0d7ab9e08511
Aayden <aaydencameron24@gmail.com>
2026-04-27 20:27:37 +1200

    feat: removed react routing for map

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -13,7 +13,6 @@ createRoot(document.getElementById("root")!).render(
     <DesktopOnly>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/construction" element={<UnderConstruction />} />
         <Route path="/spotlight" element={<UnderConstruction />} />
         <Route path="/handbook" element={<Handbook />} />
         <Route path="/map" element={<UnderConstruction />} />
